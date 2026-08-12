3c44b4f91fe579facfcb9470300dcce0ed9b4ab1
ridahafeez786 <ridahafeez786@gmail.com>
2026-08-12 16:22:44 +0500

    update

--- src/prepare_data.py
+++ src/prepare_data.py
@@ -1,21 +1,30 @@
+import os
+os.environ.setdefault('OPENBLAS_NUM_THREADS', '1')
+os.environ.setdefault('OMP_NUM_THREADS', '1')
+os.environ.setdefault('MKL_NUM_THREADS', '1')
+
 import pandas as pd
 from sklearn.datasets import load_wine
 from sklearn.model_selection import train_test_split
 import yaml
-import os
+
 # Read config
 with open('params.yaml', 'r') as f:
- params = yaml.safe_load(f)
+    params = yaml.safe_load(f)
+
 test_size = params['data']['test_size']
 random_state = params['data']['random_state']
+
 # Load wine dataset
 wine = load_wine()
 df = pd.DataFrame(wine.data, columns=wine.feature_names)
 df['target'] = wine.target
+
 # Split
 train_df, test_df = train_test_split(
- df, test_size=test_size, random_state=random_state
+    df, test_size=test_size, random_state=random_state
 )
+
 # Save
 os.makedirs('data', exist_ok=True)
 train_df.to_csv('data/train.csv', index=False)
--- src/train.py
+++ src/train.py
@@ -1,3 +1,8 @@
+import os
+os.environ.setdefault('OPENBLAS_NUM_THREADS', '1')
+os.environ.setdefault('OMP_NUM_THREADS', '1')
+os.environ.setdefault('MKL_NUM_THREADS', '1')
+
 import pandas as pd
 import yaml
 from sklearn.linear_model import LogisticRegression
@@ -5,32 +10,38 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 import pickle
-import os
+
 # Load params
 with open('params.yaml', 'r') as f:
- params = yaml.safe_load(f)
+    params = yaml.safe_load(f)
+
 model_type = params['model']['type']
 model_params = params['model']['params']
+
 # Load training data
 train_df = pd.read_csv('data/train.csv')
 X_train = train_df.drop('target', axis=1)
 y_train = train_df['target']
+
 # Select model based on config
 if model_type == 'logistic_regression':
- model = LogisticRegression(**model_params)
+    model = LogisticRegression(**model_params)
 elif model_type == 'decision_tree':
- model = DecisionTreeClassifier(**model_params)
+    model = DecisionTreeClassifier(**model_params)
 elif model_type == 'random_forest':
- model = RandomForestClassifier(**model_params)
+    model = RandomForestClassifier(**model_params)
 elif model_type == 'svm':
- model = SVC(**model_params)
+    model = SVC(**model_params)
 else:
- raise ValueError(f'Unknown model type: {model_type}')
+    raise ValueError(f'Unknown model type: {model_type}')
+
 # Train
 model.fit(X_train, y_train)
+
 # Save model locally for DVC tracking
 os.makedirs('models', exist_ok=True)
 with open('models/model.pkl', 'wb') as f:
- pickle.dump(model, f)
+    pickle.dump(model, f)
+
 print(f'Model trained: {model_type}')
 print(f'Training samples: {len(X_train)}')
